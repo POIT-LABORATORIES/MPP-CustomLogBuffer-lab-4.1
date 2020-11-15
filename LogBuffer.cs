be23b0826f727911df44a0ff5b61c28e39919fd4
Pluralment <Foolman.ru@yandex.ru>
2020-11-15 22:59:11 +0300

    update

---
+++ LogBuffer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CustomLogBuffer
+{
+    public class LogBuffer
+    {
+        //private List<string> _buffer = new List<string>();
+        //private ConcurrentQueue<string> _queue = new ConcurrentQueue<string>();
+        //private BlockingCollection<string> _blockingCollection = new BlockingCollection<string>();
+        private BlockingCollection<string> _blockingCollection;
+        private int MaxBufferSize
+        {
+            get;
+        }
+
+        public LogBuffer(int num)
+        {
+            MaxBufferSize = num;
+            _blockingCollection = new BlockingCollection<string>(MaxBufferSize);
+        }
+
+        public void Add(string item)
+        {
+            try
+            {
+                if (_blockingCollection.IsCompleted) return;
+                _blockingCollection.Add(item);
+                if (_blockingCollection.Count != MaxBufferSize) return;
+                var writeToFileTask = Task.Run(WriteToFile);
+                Task.WaitAll(writeToFileTask);
+            }
+            catch (ObjectDisposedException e)
+            {
+                Console.WriteLine("The blocking collection has been disposed of, cannot add any more messages!");
+                throw;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private void WriteToFile()
+        {
+            Console.WriteLine("WRITING TO FILE");
+            using (StreamWriter streamWriter = new StreamWriter(@"./LogFile.txt", true))
+            {
+                while (_blockingCollection.Count != 0)
+                {
+                    string item;
+                    while (_blockingCollection.TryTake(out item))
+                    {
+                        streamWriter.WriteLine(item);
+                    }
+                }
+            }
+        }
+
+        public void CompleteJournaling()
+        {
+            _blockingCollection.CompleteAdding();
+            _blockingCollection.Dispose(); // МОЖЕТ ВОЗНИКНУТЬ ОШИБКА!
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CustomLogBuffer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoJournaling();
+            Console.WriteLine("Enter any letter to exit...");
+            Console.ReadKey();
+        }
+
+        static void DoJournaling()
+        {
+            var message = "";
+            var logBuffer = new LogBuffer(5);
+            while (message != @"\e")
+            {
+                Console.WriteLine(@"Enter '\e' to exit");
+                Console.Write("Enter message: ");
+                message = Console.ReadLine();
+                if (message != "")
+                {
+                    var item = message;
+                    Task.Run(() => logBuffer.Add(item));
+                }
+            }
+            logBuffer.CompleteJournaling();
+        }
+    }
+}
